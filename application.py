01bcdf856f49a5bbcf5e94ab8be023fe25b8c349
Cristopher Estrada <sircestrada@gmail.com>
2026-05-05 20:42:12 -0600

    Initial commit with Flask app and necessary configurations

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+@application.route('/')
+def hello():
+    return 'Hello, AWS Flask App!'
+
+if __name__ == "__main__":
+    application.run(debug=False, host='0.0.0.0', port=5000)
--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 import pytest
-from app import app
+from application import app
 
 @pytest.fixture
 def client():
