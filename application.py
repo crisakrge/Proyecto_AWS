799f2683bc7af760b0cbda699165aa98512f2f4b
Cristopher Estrada <sircestrada@gmail.com>
2026-05-06 09:49:01 -0600

    Updated Flask app message

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return 'Hello, AWS Flask App!'
+    return 'Hello, AWS Flask App! (Update)'
 
 if __name__ == "__main__":
     application.run(debug=False, host='0.0.0.0', port=5000)
